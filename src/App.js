22bf764bb8717b07fd35c29cf7930d3cb14d166b
Ahmad7264 <dilshadahmad286@gmail.com>
2024-01-29 17:20:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useState } from 'react';
+import  "./App.css";  
+import DeleteIcon from "@mui/icons-material/Delete";
+import { IconButton } from '@mui/material';
 function App() {
+  
+  const [todoText, setTodoText] = useState("");
+  const [todoList, setTodoList] =useState([]);
+
+  const handleChange = ({ target }) => {
+      setTodoText(target.value);
+  };
+
+  console.log(todoList);
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    setTodoList ([...todoList, todoText]);
+    setTodoText("");
+  };
+
+  const deleteTodo = (index) => {
+    todoList.splice(index, 1)
+    setTodoList([...todoList, todoText])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Timer */}
+      <header className='App-header'>
+        <h1>TO DO</h1>
       </header>
+      <form onSubmit={handleSubmit}>
+        <label className='todo-label'>What do you want to do</label>
+        <br/>
+        <input value={todoText} className='input' type='text' 
+        onChange={(event) => handleChange(event) } 
+        
+        />
+        <input className='button' type='submit' value= 'Submit' />
+
+      </form> 
+      {
+        todoList.map((td, index) => (
+          td !== "" && (
+            <>
+            <div className='todo'>
+        <br/>
+        <input type='checkbox' />
+        <label>{td}</label>
+        <IconButton onClick={() => deleteTodo(index)}>
+        <DeleteIcon/>
+        </IconButton>
+      </div>
+            </>
+          )
+        ))
+      }
+     
+    
     </div>
   );
 }
